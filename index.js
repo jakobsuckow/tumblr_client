b4a3e1cfb5faa28023ca5c8e97002d7d7570293f
jakobsuckow <jakob@martian.agency>
2019-10-15 22:04:38 +0200

    webpack 0.5

--- index.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config()
-const baseUrl = `api.tumblr.com/v2/blog/jakobsuckow.tumblr.com/posts?api_key=`
-const apiKey = process.env.TUMBLR_API_KEY
-const url = `${baseUrl}${apiKey}`
-
